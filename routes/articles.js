8dac547b58068c43803fe631c09beb4ca5ba7c3d
Brett <brett_matsumoto@yahoo.com>
2019-04-24 16:32:23 -1000

    fixxed res.render path

--- routes/articles.js
+++ routes/articles.js
@@ -3,20 +3,21 @@
 const express = require('express');
 const router = express();
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const articleRoute = require('../database/articles.js');
 const PORT = 8000;
 
+router.get('/', (req, res) => {
+  articleRoute.getAllArticles();
+  res.render('templates/articles/article', articleRoute.getAllArticles());
+});
 router.post('/', (req, res) => {
   articleRoute.postArticle(req, res);
 });
-
 router.put('/:title', (req, res) => {
   articleRoute.putArticle(req, res);
 });
-
 router.delete('/:title', (req, res) => {
   articleRoute.deleteArticle(req, res);
-})
+});
 
 module.exports = router;
