a7380c43ffbc494a07bd752f1492d9e112606857
Brett <brett_matsumoto@yahoo.com>
2019-04-24 16:33:08 -1000

    changed hbs engine extension

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({ extended: true });
 const articleRoute = require('./routes/articles.js');
 const productRoute = require('./database/products.js');
@@ -11,7 +11,9 @@ const PORT = 8000;
 
 app.use(urlEncodedParser)
 app.use('/articles', articleRoute);
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const server = app.listen(PORT, () => {
-  console.log(`Express is running at port ${PORT}.`)
+  console.log(`Express is running at port ${PORT}.`);
 })
