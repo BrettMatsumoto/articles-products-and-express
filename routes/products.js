ce6fec05191bfcc758eec134ea3eb6cc5dcdd959
Brett <brett_matsumoto@yahoo.com>
2019-04-25 20:31:37 -1000

    safety commit

--- routes/products.js
+++ routes/products.js
@@ -7,27 +7,27 @@ let count = 0;
 
 database
   .route('/')
-  .get((req, res) => {
-    res.render('templates/products/index', productRoute.getAllProducts());
-  })
-  .post((req, res) => {
-    count++;
-    productRoute.postProduct(count, req.body.name, req.body.price, req.body.inventory, res);
-    res.send({ success: true });
-  });
+    .get((req, res) => {
+      res.render('templates/products/index', productRoute.getAllProducts());
+    })
+    .post((req, res) => {
+      count++;
+      productRoute.postProduct(count, req.body.name, req.body.price, req.body.inventory);
+      res.send({ success: true });
+    });
 
 database
   .route('/:id')
-  .get((req, res) => {
-    res.render('templates/products/products', productRoute.getSpecificProduct(req.params.id));
-  })
-  .put((req, res) => {
-    productRoute.putProduct(req.url, req.body.title, req.body.price, req.body.inventory);
-    res.send({ success: true });
-  })
-  .delete((req, res) => {
-    productRoute.deleteProduct(req.body.id);
-    res.send({ success: true });
-  });
+    .get((req, res) => {
+      res.render('templates/products/products', productRoute.getSpecificProduct(req.params.id));
+    })
+    .put((req, res) => {
+      productRoute.putProduct(req.params.id, req.body.title, req.body.price, req.body.inventory);
+      res.send({ success: true });
+    })
+    .delete((req, res) => {
+      productRoute.deleteProduct(req.body.id);
+      res.send({ success: true });
+    });
 
 module.exports = database;
