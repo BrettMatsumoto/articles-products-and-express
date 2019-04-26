50d9048c9168f4aa8875cbef56ec995c2916e89c
Brett <brett_matsumoto@yahoo.com>
2019-04-25 21:05:19 -1000

    adjusted without prettier

--- routes/products.js
+++ routes/products.js
@@ -5,29 +5,27 @@ const database = express.Router();
 const productRoute = require('../database/products');
 let count = 0;
 
-database
-  .route('/')
-    .get((req, res) => {
-      res.render('templates/products/index', productRoute.getAllProducts());
-    })
-    .post((req, res) => {
-      count++;
-      productRoute.postProduct(count, req.body.name, req.body.price, req.body.inventory);
-      res.send({ success: true });
-    });
+database.route('/')
+  .get((req, res) => {
+    res.render('templates/products/index', productRoute.getAllProducts());
+  })
+  .post((req, res) => {
+    count++;
+    productRoute.postProduct(count, req.body.name, req.body.price, req.body.inventory);
+    res.send({ success: true });
+  });
 
-database
-  .route('/:id')
-    .get((req, res) => {
-      res.render('templates/products/products', productRoute.getSpecificProduct(req.params.id));
-    })
-    .put((req, res) => {
-      productRoute.putProduct(req.params.id, req.body.name, req.body.price, req.body.inventory);
-      res.send({ success: true });
-    })
-    .delete((req, res) => {
-      productRoute.deleteProduct(req.params.id);
-      res.send({ success: true });
-    });
+database.route('/:id')
+  .get((req, res) => {
+    res.render('templates/products/products', productRoute.getSpecificProduct(req.params.id));
+  })
+  .put((req, res) => {
+    productRoute.putProduct(req.params.id, req.body.name, req.body.price, req.body.inventory);
+    res.send({ success: true });
+  })
+  .delete((req, res) => {
+    productRoute.deleteProduct(req.params.id);
+    res.send({ success: true });
+  });
 
 module.exports = database;
