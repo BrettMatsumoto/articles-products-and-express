abe73358ed6ea1655fb0e2462a6aa29b183415b4
Brett <brett_matsumoto@yahoo.com>
2019-04-25 20:31:16 -1000

    made a real RMP

--- database/products.js
+++ database/products.js
@@ -1,55 +1,48 @@
 'use strict';
-
-let products = { products: [] };
-
-const getAllProducts = () => {
-  return products;
-};
-
-const postProduct = (count, reqName, reqPrice, reqInventory) => {
-  let product = { id: count, name: reqName, price: reqPrice, inventory: reqInventory };
-  products.products.push(product);
-  console.log(products)
-};
-
-const getSpecificProduct = (req) => {
-  // for (let i = 0; i <= products.products.length; i++) {
-  //   console.log('testtesttest', products.products[i].id === req)
-  //   if (products.products[i].id === req) {
-  //     return products.products[i];
-  //   }
-  // }
-  let specificProduct;
-  products.products.forEach(product => {
-    if (product.id === req) {
-      specificProduct = product;
+(function RMP() {
+  let products = { products: [] };
+  
+  const getAllProducts = () => {
+    return products;
+  };
+  
+  const postProduct = (count, reqName, reqPrice, reqInventory) => {
+    let product = { id: count, name: reqName, price: reqPrice, inventory: reqInventory };
+    products.products.push(product);
+  };
+  
+  const getSpecificProduct = (req) => {
+    for (let i = 0; i <= products.products.length-1; i++) {
+      if (parseInt(products.products[i].id) === parseInt(req)) {
+        return products.products[i];
+      }
     }
-  })
-  return specificProduct;
-};
-
-const putProduct = (reqUrl, reqName, reqPrice, reqInventory) => {
-  for (let i = 0; i < products.products.length; i++) {
-    if (`/${products.products[i].id}` === reqUrl) {
-      products.products[i].title = reqTitle;
-      products.products[i].body = reqBody;
-      products.products[i].author = reqAuthor;
+  };
+  
+  const putProduct = (reqId, reqName, reqPrice, reqInventory) => {
+    for (let i = 0; i < products.products.length; i++) {
+      if (parseInt(products.products[i].id) === parseInt(reqId)) {
+        products.products[i].title = reqName;
+        products.products[i].body = reqPrice;
+        products.products[i].author = reqInventory;
+      }
     }
-  }
-};
-
-const deleteProduct = (req) => {
-  for (let i = 0; i < products.products.length; i++) {
-    if (products.products[i].id === req) {
-      products.products.splice(i, 1);
+  };
+  
+  const deleteProduct = (req) => {
+    for (let i = 0; i < products.products.length; i++) {
+      if (products.products[i].id === req) {
+        products.products.splice(i, 1);
+      }
     }
-  }
-};
+  };
+  
+  module.exports = {
+    getAllProducts,
+    getSpecificProduct,
+    postProduct,
+    putProduct,
+    deleteProduct,
+  };
 
-module.exports = {
-  getAllProducts,
-  getSpecificProduct,
-  postProduct,
-  putProduct,
-  deleteProduct,
-};
+})();
